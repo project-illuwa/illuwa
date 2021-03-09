d3ee652e77f3b6578b3f9a09b4ab624e94b1663f
정윤성 <123dbstjd@naver.com>
2021-03-09 18:02:12 +0900

    init

---
+++ src/test/java/com/yunseong/illuwa/IlluwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunseong.illuwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlluwaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

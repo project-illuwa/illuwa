85bd45ca6212a3a97f0032485de3db7a7626092e
정윤성 <123dbstjd@naver.com>
2021-03-11 01:27:52 +0900

    MODIFY Gradle & ADD Illuwa-dev.yml, Discovery Server, Config Server

--- Image/src/main/java/com/yunseong/image/ImageApplication.java
+++ Image/src/main/java/com/yunseong/image/ImageApplication.java
@@ -2,8 +2,10 @@ package com.yunseong.image;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ImageApplication {
 
     public static void main(String[] args) {

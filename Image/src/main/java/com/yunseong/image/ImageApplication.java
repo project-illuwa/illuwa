71b61ab4a985649a2f7671d6ed31ba84ab72faa3
정윤성 <123dbstjd@naver.com>
2021-03-11 22:40:05 +0900

    DEL APIGateway bootstrap.yml, gateway-dev.yml, ADD APIGateway application.yml

--- Image/src/main/java/com/yunseong/image/ImageApplication.java
+++ Image/src/main/java/com/yunseong/image/ImageApplication.java
@@ -3,8 +3,6 @@ package com.yunseong.image;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,14 +10,5 @@ public class ImageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImageApplication.class, args);
-    }
-}
-
-@RestController
-class Test {
-
-    @GetMapping(value = "/")
-    public String test() {
-        return "Good";
     }
 }

14db170ff99fb24dcc56b606ea92ecca92c1b85e
정윤성 <123dbstjd@naver.com>
2021-03-12 19:57:43 +0900

    ADD env = cloud, db, pw

--- Image/src/main/java/com/yunseong/image/ImageApplication.java
+++ Image/src/main/java/com/yunseong/image/ImageApplication.java
@@ -3,6 +3,8 @@ package com.yunseong.image;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +12,14 @@ public class ImageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImageApplication.class, args);
+    }
+}
+
+@RestController
+class Test{
+
+    @GetMapping("/")
+    public String test() {
+        return "Good!";
     }
 }
